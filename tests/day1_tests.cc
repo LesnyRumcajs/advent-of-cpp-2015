a51769d513b51fafd80c3638fbf5952fe3eb225a
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2021-09-29 20:54:11 +0200

    added tests, refactored project structure

---
+++ tests/day1_tests.cc
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "../src/day1.h"
+
+
+TEST_CASE("Day1::part1", "[findTargetFloor]") {
+    REQUIRE(0 == day1::findTargetFloor(""));
+    REQUIRE(2 == day1::findTargetFloor("()()(("));
+}
+
+TEST_CASE("Day1::part2", "[findFirstBasementFloor]") {
+    REQUIRE(1 == day1::findFirstBasementFloor(")"));
+    REQUIRE(5 == day1::findFirstBasementFloor("(()))"));
+}
